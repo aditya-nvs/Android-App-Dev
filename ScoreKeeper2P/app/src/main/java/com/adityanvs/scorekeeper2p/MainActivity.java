190d8f03680d44151f0b7c7a391fe41df4f9f418
aditya-nadamuni <aditya.nadamuni@gmail.com>
2020-08-04 00:25:41 +0530

    added functions to make workable

--- ScoreKeeper2P/app/src/main/java/com/adityanvs/scorekeeper2p/MainActivity.java
+++ ScoreKeeper2P/app/src/main/java/com/adityanvs/scorekeeper2p/MainActivity.java
@@ -3,12 +3,42 @@ package com.adityanvs.scorekeeper2p;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView p1, p2;
+    private int counter1, counter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p1 = findViewById(R.id.scoreP1);
+        p2 = findViewById(R.id.scoreP2);
+        counter1 = 0;
+        counter2 = 0;
+    }
+
+    public void decrementScore1(View view) {
+        counter1--;
+        p1.setText(String.valueOf(counter1));
+    }
+
+    public void incrementScore1(View view) {
+        counter1++;
+        p1.setText(String.valueOf(counter1));
+    }
+
+
+    public void decrementScore2(View view) {
+        counter2--;
+        p2.setText(String.valueOf(counter2));
+    }
+
+    public void incrementScore2(View view) {
+        counter2++;
+        p2.setText(String.valueOf(counter2));
     }
 }
